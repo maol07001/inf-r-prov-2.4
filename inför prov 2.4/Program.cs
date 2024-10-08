3b8bd74310e8997a11f9e82b8d20ac18591302c5
maol07001 <maol07001@E-PW05CGR4>
2024-10-08 14:36:13 +0200

    Add project files.

---
+++ inför prov 2.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace uppgift_2._3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad har den första anställda för lön");
+            int lön1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vad har den andra anställda för lön");
+            int lön2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vad har den tredje anställda för lön");
+            int lön3 = int.Parse(Console.ReadLine());
+
+            int sum = (lön1 + lön2 + lön3) / 3;
+            Console.WriteLine("Medelvärdet av de tre lönerna är" + sum);
+
+        }
+    }
+}
